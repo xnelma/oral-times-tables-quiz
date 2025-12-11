5f631bc0f229144d3291ed68b941076121321475
xnelma <151833954+xnelma@users.noreply.github.com>
2025-12-11 16:16:14 +0100

    chore: extend unit test for auto locale

--- test/auto_locale_test.cpp
+++ test/auto_locale_test.cpp
@@ -3,16 +3,45 @@
 #include <QLocale>
 #include <gtest/gtest.h>
 
-TEST(AutoLocaleTest, FindsMatchingResource)
+TEST(AutoLocaleTest, FindsUniqueResource)
 {
-    auto match = Tts::LocaleDescriptor(QLocale::English, QLocale::Germany);
+    TestTranslator::resources().clear();
 
+    // Prepare one entry that also is the match.
+    auto match = Tts::LocaleDescriptor(QLocale::English, QLocale::UnitedStates);
     QLocale::setDefault(QLocale(match.language, match.territory));
     TestTranslator::resources().insert({ match, "" });
+    auto expected = match;
 
     Tts::LocaleDescriptor result = Tts::autoLocale<TestTranslator>();
+    EXPECT_EQ(result, expected);
 
-    auto expected = match;
+    // Insert more entries, all different in locale and territory; keep 'match'
+    // the same.
+    TestTranslator::resources().insert(
+        { { Tts::LocaleDescriptor(QLocale::German, QLocale::Germany), "" },
+          { Tts::LocaleDescriptor(QLocale::French, QLocale::France), "" } });
+
+    result = Tts::autoLocale<TestTranslator>();
+    EXPECT_EQ(result, expected);
+
+    // Insert another entry and change 'match'.
+    TestTranslator::resources().insert(
+        { Tts::LocaleDescriptor(QLocale::Italian, QLocale::Italy), "" });
+    match.language = QLocale::French;
+    match.territory = QLocale::France;
+    QLocale::setDefault(QLocale(match.language, match.territory));
+    expected = match;
+
+    result = Tts::autoLocale<TestTranslator>();
+    EXPECT_EQ(result, expected);
+}
+
+TEST(AutoLocaleTest, RunsWithEmptyResources)
+{
+    TestTranslator::resources().clear();
+    Tts::LocaleDescriptor expected; // c-Locale
+    Tts::LocaleDescriptor result = Tts::autoLocale<TestTranslator>();
 
     EXPECT_EQ(result, expected);
 }
