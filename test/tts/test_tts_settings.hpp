a64ff57ff88ab907489563eacd82376f41a93e04
xnelma <151833954+xnelma@users.noreply.github.com>
2026-01-12 14:27:29 +0100

    chore: add test implementation for settings

---
+++ test/tts/test_tts_settings.hpp
@@ -0,0 +1,44 @@
+#ifndef OTTQ_20251222_1939_INCLUDE
+#define OTTQ_20251222_1939_INCLUDE
+
+#include "abstract_tts_settings.hpp"
+#include "locale_descriptor.hpp"
+#include "test_translation_resources.hpp"
+#include "auto_locale.hpp"
+
+class TestSettings : public Tts::AbstractSettings
+{
+private:
+    bool useAutoLocale_{ false };
+    double voiceRate_{ 0 };
+
+    Tts::AutoLocale<TestTranslationResources> autoLocale_;
+
+    Tts::LocaleDescriptor &getAutoLocale() override
+    {
+        autoLocale_ = Tts::AutoLocale<TestTranslationResources>();
+        return autoLocale_;
+    }
+
+public:
+    TestSettings() { }
+
+    Tts::LocaleDescriptor loadLocaleSetting() override { return getLocale(); }
+    bool loadAutoLocaleSetting() override { return useAutoLocale_; }
+    double loadVoiceRateSetting() override { return voiceRate_; }
+
+    void saveLocaleSetting(const Tts::LocaleDescriptor &ld) override
+    {
+        setLocale(ld);
+    }
+    void saveAutoLocaleSetting(const bool useAutoLocale) override
+    {
+        useAutoLocale_ = useAutoLocale;
+    }
+    void saveVoiceRateSetting(const double voiceRate) override
+    {
+        voiceRate_ = voiceRate;
+    }
+};
+
+#endif // OTTQ_20251222_1939_INCLUDE
