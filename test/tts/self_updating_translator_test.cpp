cc4177fe3473c94037461b10a46c3008ad0615f5
xnelma <151833954+xnelma@users.noreply.github.com>
2026-01-13 15:18:53 +0100

    chore: test translator update for system locale change

--- test/tts/self_updating_translator_test.cpp
+++ test/tts/self_updating_translator_test.cpp
@@ -77,3 +77,51 @@ TEST(TranslatorUpdaterTest, CatchesSettingsUpdate)
     translatorLocale = translator.localeDescriptor();
     EXPECT_EQ(translatorLocale, newCur);
 }
+
+TEST(TranslatorUpdaterTest, CatchesSystemLocaleUpdate)
+{
+    TestTranslationResources::get().clear();
+    auto sys = Tts::LocaleDescriptor(QLocale::English, QLocale::UnitedStates);
+    auto curEn = Tts::LocaleDescriptor(QLocale::English, QLocale::AnyTerritory);
+    auto curDe = Tts::LocaleDescriptor(QLocale::German, QLocale::AnyTerritory);
+    QLocale::setDefault(QLocale(sys.language, sys.territory));
+    TestTranslationResources::get().insert(
+        { curEn, TtsTest::ResourcePaths::en });
+    TestTranslationResources::get().insert(
+        { curDe, TtsTest::ResourcePaths::de });
+
+    std::shared_ptr<TestSettings> settings = std::make_shared<TestSettings>();
+    settings->saveAutoLocaleSetting(true);
+    EXPECT_EQ(settings->resolvedLocale(), sys);
+    EXPECT_EQ(settings->resolvedLocale().resourceKey(), curEn);
+
+    Tts::SelfUpdatingTranslator<TtsTest::Translator, TestTranslationResources>
+        translator(settings);
+    std::unordered_map<QString, TtsTest::Locale> translations;
+    translations.insert({ TtsTest::ResourcePaths::en, TtsTest::Locale::En });
+    translations.insert({ TtsTest::ResourcePaths::de, TtsTest::Locale::De });
+    translator.setup(translations, TtsTest::ResourcePaths::en);
+
+    Tts::LocaleDescriptor translatorLocale = translator.localeDescriptor();
+    EXPECT_EQ(translatorLocale, curEn);
+
+    const char *test = "test";
+    QString res = translator.translate(nullptr, test);
+    std::string testStr{ test };
+    TtsTest::Translator::permutate(testStr, TtsTest::Locale::En);
+    EXPECT_EQ(res, QString::fromStdString(testStr));
+
+    // update system locale
+    auto newSys = Tts::LocaleDescriptor(QLocale::German, QLocale::Germany);
+    QLocale::setDefault(QLocale(newSys.language, newSys.territory));
+    EXPECT_EQ(settings->resolvedLocale(), newSys);
+    EXPECT_EQ(settings->resolvedLocale().resourceKey(), curDe);
+
+    res = translator.translate(nullptr, test);
+    testStr = test;
+    TtsTest::Translator::permutate(testStr, TtsTest::Locale::De);
+    EXPECT_EQ(res, QString::fromStdString(testStr));
+
+    translatorLocale = translator.localeDescriptor();
+    EXPECT_EQ(translatorLocale, curDe);
+}
