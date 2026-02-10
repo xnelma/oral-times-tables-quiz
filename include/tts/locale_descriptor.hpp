b59302ce796b3a6779b3c1c42173872b5212d540
xnelma <151833954+xnelma@users.noreply.github.com>
2026-02-10 20:58:05 +0100

    Revert "chore: add formatter specialization for locale descriptor"
    This reverts commit da2bf5a7370af8c5ad6707210258154c7ac6d5e8.
    
    The google test output requires << operator overload, so keeping
    both a specialization for use with std::formatter and ostream
    might not make sense.
    This project therefore should only use stream-based output for now.

--- include/tts/locale_descriptor.hpp
+++ include/tts/locale_descriptor.hpp
@@ -3,7 +3,6 @@
 
 #include <QLocale>
 #include <ostream>
-#include <format>
 
 namespace Tts {
 
@@ -35,23 +34,5 @@ struct LocaleDescriptor
 std::ostream &operator<<(std::ostream &os, const Tts::LocaleDescriptor &ld);
 
 } // namespace Tts
-
-template <class CharT>
-struct std::formatter<Tts::LocaleDescriptor, CharT>
-    : std::formatter<std::string, CharT>
-{
-    template <typename FormatContext>
-    auto format(Tts::LocaleDescriptor &ld, FormatContext &fc) const
-    {
-        auto languageStr = QLocale::languageToString(ld.language);
-        auto territoryStr = QLocale::territoryToString(ld.territory);
-
-        return std::formatter<string>::format(
-            std::format("({}, {})",
-                        languageStr.toUtf8().data(),
-                        territoryStr.toUtf8().data()),
-            fc);
-    }
-};
 
 #endif // OTTQ_20250829_1805_INCLUDE
