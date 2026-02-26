5fcf81f76df3d0e89431129b173b85ce4bccc13b
xnelma <151833954+xnelma@users.noreply.github.com>
2026-02-26 18:18:53 +0100

    chore: add unit test for resource path parsing

---
+++ test/tts/from_resourcepath_test.cpp
@@ -0,0 +1,28 @@
+#include "locale_descriptor.hpp"
+#include <gtest/gtest.h>
+
+TEST(FromResourcePathTest, DefaultForEmptyPath)
+{
+    Tts::LocaleDescriptor result, expected;
+    result = Tts::LocaleDescriptor::fromResourcePath("");
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(FromResourcePathTest, DefaultForMissingFilename)
+{
+    Tts::LocaleDescriptor result, expected;
+    result = Tts::LocaleDescriptor::fromResourcePath("test/");
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(FromResourcePathTest, ParsesValidPath)
+{
+    Tts::LocaleDescriptor result, expected;
+    result = Tts::LocaleDescriptor::fromResourcePath(
+        "test/en_DE." TRANSLATION_FILE_ENDING);
+
+    expected = Tts::LocaleDescriptor(QLocale::English, QLocale::Germany);
+    EXPECT_EQ(result, expected);
+}
